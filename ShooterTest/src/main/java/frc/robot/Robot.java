538e4ce57cc3a0879a71b48b2d92dbc161dbabbc
Alex Rummel <rummelalexander@outlook.com>
2022-02-11 19:28:32 -0500

    changed to limiters

--- ShooterTest/src/main/java/frc/robot/Robot.java
+++ ShooterTest/src/main/java/frc/robot/Robot.java
@@ -12,27 +12,28 @@ import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
+
+
   private Joystick controller;
   private static final int deviceID = 5;
   private CANSparkMax m_motor;
   private Spark n_motor;
-  private double leftSpeedLimit = .68;
-  private double rightSpeedLimit = .57;
- private double deadZone = .02;
+  private double deadZone = .02; 
+
+  
 
   @Override
   public void robotInit() {
-    SmartDashboard.putNumber("Spark Max Set Speed", .0);
-    SmartDashboard.putNumber("Spark Set Speed", .0);
+    SmartDashboard.putNumber("Spark Max Limit 0-1", .0);
+    SmartDashboard.putNumber("Spark Limit 0-1", .0);
     
     m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
-    //SendableRegistry.setName(m_motor, "Info Slider", 5);
 
     n_motor = new Spark(1);
-    SendableRegistry.setName(n_motor, "Info Slider", "Spark Speed Slider");
 
 
     controller = new Joystick(0); //Left Joystick fore/aft
@@ -42,8 +43,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("Spark_Max", m_motor.get());
-    SmartDashboard.putNumber("Spark", n_motor.get());
+    SmartDashboard.putNumber("Spark_Max Current Speed", m_motor.get());
+    SmartDashboard.putNumber("Spark Current Speed", n_motor.get());
     
     //SmartDashboard.setDefaultNumber("Motor M Speed", leftSpeedLimit);
     //leftSpeedLimit = SmartDashboard.getDefaultNumber("Motor M", leftSpeedLimit);
@@ -65,15 +66,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    //if (Math.abs(controller.getRawAxis(1)) < deadZone) m_motor.set(0); //Sets the dead zone so that the motor stops
-    //if (Math.abs(controller.getRawAxis(5)) < deadZone) n_motor.set(0);
-    //m_motor.set(controller.getRawAxis(1)*leftSpeedLimit); //Set SparkMax to left joystick input
-    //n_motor.set(controller.getRawAxis(5)*rightSpeedLimit);
-    double motorSpeedM = SmartDashboard.getNumber("Spark Max Set Speed", .1);
-    m_motor.set(motorSpeedM);
-    double motorSpeedN = SmartDashboard.getNumber("Spark Set Speed", .1);
-    n_motor.set(motorSpeedN);
-    //n_motor.set(controller.getRawAxis(5)*rightSpeedLimit);
+    double motorSparkLimit = SmartDashboard.getNumber("Spark Limit 0-1", .1);
+    double motorSparkMaxLimit = SmartDashboard.getNumber("Spark Max Limit 0-1", .1);
+    if (Math.abs(controller.getRawAxis(1)) < deadZone) m_motor.set(0); //Sets the dead zone so that the motor stops
+    if (Math.abs(controller.getRawAxis(5)) < deadZone) n_motor.set(0);
+    m_motor.set(controller.getRawAxis(1)*motorSparkMaxLimit); //Set SparkMax to left joystick input
+    n_motor.set(controller.getRawAxis(5)*motorSparkLimit);
   }
 
   @Override
@@ -88,3 +86,4 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {}
 }
+
